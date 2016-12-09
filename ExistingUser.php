756b8ee6b53e0a5de42555e5d303601903458826
Austin Juhl <a002j556@ku.edu>
2016-12-08 18:38:10 -0600

    fix cookie.

--- ExistingUser.php
+++ ExistingUser.php
@@ -5,7 +5,7 @@ $user = $_POST['username'];
 $password = $_POST['password'];
 $query = "SELECT user_id,user_pw FROM Users";
 $result = $conn->query($query);
-setcookie("user", $user, time() + 43200);
+
 
 echo "<html>
 			<head><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'></head>
@@ -19,6 +19,7 @@ $exists = 0;
 								<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/HealthFunctions.php' class='btn btn-info' role='button'>Calculate Macros</a>
 								<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/Saved.html' class='btn btn-info' role='button'>View Saved MacroPreps</a>";
 								$exists = 1;
+								setcookie("user", $user, time() + 43200, '/', null, true, true );
 			return;
 		}
 		else if($row['user_id'] == $user && $row['user_pw'] == $password){
