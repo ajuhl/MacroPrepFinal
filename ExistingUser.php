3e7e6df84a40d52c14a9ff58562a5eb3fefe71e9
Austin Juhl <a002j556@ku.edu>
2016-12-08 19:57:17 -0600

    view users saved meals

--- ExistingUser.php
+++ ExistingUser.php
@@ -17,7 +17,7 @@ $exists = 0;
 			echo "<p>Welcome Back ".$user."!</p><br>
 								<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/MealPlanner.html' class='btn btn-info' role='button'>Input Macros</a>
 								<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/HealthFunctions.php' class='btn btn-info' role='button'>Calculate Macros</a>
-								<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/Saved.html' class='btn btn-info' role='button'>View Saved MacroPreps</a>";
+								<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/Saved.php' class='btn btn-info' role='button'>View Saved MacroPreps</a>";
 								$exists = 1;
 								setcookie("user", $user, time() + 43200, '/', null, true, true );
 			return;
