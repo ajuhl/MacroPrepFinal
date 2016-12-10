dc54c1e47f42ea146811c638aac7560e485e2207
ajuhl <a002j556@ku.edu>
2016-12-10 15:54:29 -0600

    css

--- ExistingUser.php
+++ ExistingUser.php
@@ -7,38 +7,117 @@ $query = "SELECT user_id,user_pw FROM Users";
 $result = $conn->query($query);
 
 
-echo "<html>
-			<head><link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'></head>
-				<body>";
+echo "<!DOCUMENT>
+<html>
+<style>
+
+.macro {
+	top: 300px;
+	left: 700px;
+	margin: auto;
+	border-radius: 15px 50px 30px;
+	padding: 20px;
+	border: 2px solid black;
+	width: 500px;
+	box-shadow: 1px 2px 4px rgba(0, 0, 0, .5);
+	background: linear-gradient(135deg, rgba(252,227,0,1) 0%, rgba(255,242,173,1) 52%, rgba(252,227,0,1) 100%);
+}
+
+h1{
+	margin: auto;
+  margin-bottom: 20px;
+  border-radius: 25px;
+  border: 2px solid rgb(230, 230, 0);
+  background: linear-gradient(to bottom, rgba(0,0,0,1) 0%,rgba(25,25,25,1) 10%,rgba(25,25,25,1) 70%,rgba(51,51,51,1) 85%,rgba(102,102,102,1) 100%);
+  padding: 20px;
+  width: 450px;
+  height: 50px;
+  font-size: 40px;
+  text-align: center;
+  color: rgba(252,227,0,1);
+  border: 2px solid black;
+}
+
+.submit {
+  background-color: black;
+  color: rgba(252,227,0,1);
+  border-radius: 2px;
+  -webkit-transition-duration: 0.4s;
+  transition-duration: all 0.4s;
+  border: 1px solid black;
+  padding: 16px 16px;
+  text-align: center;
+  cursor: pointer;
+  padding: 10px;
+}
+
+input[type=text] {
+  width: 200px;
+  height: 25px;
+  -webkit-transition: width 0.4s, height 0.4s;
+  transition: width 0.4s, height 0.4s;
+  border: 2px solid black;
+  text-align: center;
+  margin: 5px;
+}
+input[type=password] {
+  width: 200px;
+  height: 25px;
+  -webkit-transition: width 0.4s, height 0.4s;
+  transition: width 0.4s, height 0.4s;
+  border: 2px solid black;
+  text-align: center;
+  margin: 5px;
+}
+
+</style>
+	<head>
+		<title>MacroPrep</title>
+	</head>
+	<body>
+		<div class='macro'>
+		<h1>MacroPrep</h1>";
 if($result->num_rows > 0){
 $exists = 0;
 	while($row = $result->fetch_assoc()){
 		if($row['user_id'] == $user && $row['user_pw'] == $password){
-			echo "<p>Welcome Back ".$user."!</p><br>
-				<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/MealPlanner.html' class='btn btn-info' role='button'>Input Macros</a>
-				<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/HealthFunctions.php' class='btn btn-info' role='button'>Calculate Macros</a>
-				<a href='https://people.eecs.ku.edu/~ajuhl/eecs448/Final/Saved.php' class='btn btn-info' role='button'>View Saved MacroPreps</a>";
-				$exists = 1;
-				setcookie("user", $user, time() + 43200, '/', null, true, true );
+			echo "<p>Welcome back ".$user."!</p><br>
+								<form action='MealPlanner.html'>
+									<input type='submit'  class='submit' value='Input Macros'>
+								</form>
+								<form action='HealthFunctions.php'>
+									<input type='submit'  class='submit' value='Calculate Macros'>
+								</form>
+								<form action='Saved.php'>
+									<input type='submit'  class='submit' value='View Saved MacroPreps'>
+								</form>";
+								$exists = 1;
+								setcookie("user", $user, time() + 43200, '/', null, true, true );
 			return;
 		}
 		else if($row['user_id'] == $user && $row['user_pw'] == $password){
-			echo "<p>Incorrect username or password<p><br>
-							<a href='https://people.eecs.ku.edu/~hcrisp/MacroPrepFinal/ExistingUser.html' class='btn btn-info' role='button'>Try again</a>";
+			echo "<p>Incorrect username or password.<p><br>
+							<form action='index.html'>
+									<input type='submit'  class='submit' value='Try Again'>
+								</form>";
 							$exists = 1;
 			return;
 		}
 	}
 	if($exists == 0){
 		echo "<p>Incorrect username or password<p><br>
-						<a href='https://people.eecs.ku.edu/~hcrisp/MacroPrepFinal/ExistingUser.html' class='btn btn-info' role='button'>Try again</a>";
+						<form action='index.html'>
+									<input type='submit'  class='submit' value='Try Again'>
+								</form>";
 	}
 }
 
 if($result->num_rows == 0){
 	$conn->query($insert);
 	echo "<p>Click 'Home' then 'New User' to create an account.</p><br>
-				<a href='https://people.eecs.ku.edu/~hcrisp/MacroPrepFinal/index.html' class='btn btn-info' role='button'>Home</a>";
+				<form action='index.html'>
+									<input type='submit'  class='submit' value='Home'>
+								</form>";
 }
 echo "</body>
 		</html>";
