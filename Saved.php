f133ec632331349cd98d84269173b977af4856a4
ajuhl <a002j556@ku.edu>
2016-12-12 12:26:37 -0600

    Format of buttons

--- Saved.php
+++ Saved.php
@@ -10,39 +10,40 @@ echo "<html>
 				table, th, td {
 					border: 1px solid black;
 					border-collapse: collapse;
+					margin:auto;
 				}
 				th, td {
 					padding: 5px;
 				}
 				th {
-					text-align: left;
+					margin-bottom: 20px;
+					border: 2px solid rgb(230, 230, 0);
+					background: linear-gradient(to bottom, rgba(0,0,0,1) 0%,rgba(25,25,25,1) 10%,rgba(25,25,25,1) 70%,rgba(51,51,51,1) 85%,rgba(102,102,102,1) 100%);
+					padding: 10px;
+					font-size: 20px;
+					text-align: center;
+					color: rgba(252,227,0,1);
+					border: 2px solid black;
+				}
+				tr{
+					background: rgba(252,227,0,1);
 				}
 				.macro {
-	top: 300px;
-	left: 700px;
-	margin: auto;
-	border-radius: 15px 50px 30px;
-	padding: 20px;
-	border: 2px solid black;
-	width: 500px;
-	box-shadow: 1px 2px 4px rgba(0, 0, 0, .5);
-	background: linear-gradient(135deg, rgba(252,227,0,1) 0%, rgba(255,242,173,1) 52%, rgba(252,227,0,1) 100%);
+					top: 300px;
+					left: 700px;
+					margin: auto;
+					border-radius: 15px 50px 30px;
+					padding: 20px;
+					border: 2px solid black;
+					width: 500px;
+					box-shadow: 1px 2px 4px rgba(0, 0, 0, .5);
+					background: linear-gradient(135deg, rgba(252,227,0,1) 0%, rgba(255,242,173,1) 52%, rgba(252,227,0,1) 100%);
 }
 
-th{
-	margin: auto;
-  margin-bottom: 20px;
-  border: 2px solid rgb(230, 230, 0);
-  background: linear-gradient(to bottom, rgba(0,0,0,1) 0%,rgba(25,25,25,1) 10%,rgba(25,25,25,1) 70%,rgba(51,51,51,1) 85%,rgba(102,102,102,1) 100%);
-  padding: 10px;
-  font-size: 20px;
-  text-align: center;
-  color: rgba(252,227,0,1);
-  border: 2px solid black;
-}
 
-tr{
-	background: rgba(252,227,0,1);
+form{
+	display: inline;
+	text-align: center;
 }
 
 .submit {
@@ -56,6 +57,9 @@ tr{
   text-align: center;
   cursor: pointer;
   padding: 10px;
+}
+.buttons{
+	text-align: center;
 }
 
 				</style>
@@ -81,11 +85,13 @@ if($result->num_rows > 0){
 if($result->num_rows == 0){
 	 echo "<p>0 results</p>";
 }
-echo "<br><form action='MealPlanner.html'>
+echo "<br><div class='buttons'>
+								<form action='MealPlanner.html'>
 									<input type='submit'  class='submit' value='Input Macros'>
 								</form>
 								<form action='HealthFunctions.php'>
 									<input type='submit'  class='submit' value='Calculate Macros'>
-								</form>";
+								</form>
+					</div>";
 $conn>close();
 ?>
